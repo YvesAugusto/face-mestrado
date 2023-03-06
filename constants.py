630705f0b3f4dde26277fbc7f7f41f82ea414e5d
yalr098 <yvesromero1998@gmail.com>
2023-03-06 12:54:09 -0300

    necessary constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+ROOT_PATH = Path().resolve()
+
+FEATURES_PATH = ROOT_PATH / "features"
+LOGGING_PATH = ROOT_PATH / "logs"
+SETTINGS_PATH = ROOT_PATH / "settings"
+
+ROOT_DATASET_FOLDER = Path("/home/yves/Área de Trabalho")
