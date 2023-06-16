3f698b15b04e3672224d717fb944dff5e5a778e5
yalr098 <yvesromero1998@gmail.com>
2023-06-16 11:09:17 -0300

    added grid file

--- constants.py
+++ constants.py
@@ -4,7 +4,9 @@ ROOT_PATH = Path().resolve()
 
 FEATURES_PATH = ROOT_PATH / "features"
 LOGGING_PATH = ROOT_PATH / "logs"
-SETTINGS_PATH = ROOT_PATH / "settings"
+GRIDS_PATH = ROOT_PATH / "grids"
+RF_GRID_PATH = GRIDS_PATH / "rf.json"
+
 FACENET_MODEL_PATH = ROOT_PATH / "models" / "inception-resnet-v1-facenet.h5"
 
 ROOT_DATASET_FOLDER = Path("/home/yves/Área de Trabalho")
